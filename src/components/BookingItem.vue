b30c9cff7fd0df93fd6737609e3bd3f4607545e9
Elfonator <elfo.here@gmail.com>
2025-08-16 01:09:06 +0200

    feat(button): add variants for RoundButton component

--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -1,10 +1,14 @@
 <template>
-    <SectionCard>
-            Booking to Vue Conference
-    </SectionCard>
+  <SectionCard>
+    <div class="flex justify-between">
+      <div>Booking to Vue Conference</div>
+      <RoundButton variant="danger" @click="$emit('cancel')"> Cancel </RoundButton>
+    </div>
+  </SectionCard>
 </template>
 
 <script setup>
-import SectionCard from '@/components/SectionCard.vue'
-
+import SectionCard from '@/components/SectionCard.vue';
+import RoundButton from '@/components/RoundButton.vue';
+defineEmits(['cancel']);
 </script>

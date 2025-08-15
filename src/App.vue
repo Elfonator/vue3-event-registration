9a9c083bcfabcd0da88f7dd158f0a85ec65fc389
Elfonator <elfo.here@gmail.com>
2025-08-16 00:51:22 +0200

    ✨ feat: initial version of event registration app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="mx-auto max-w-screen-xl my-8 space-y-8 px-4">
+    <h1 class="text-4xl">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conference 2024"
+        @register="console.log('Registered!')"
+      />
+    </section>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
+    <section class="grid grid-cols-1 gap-4">
+      <BookingItem v-for="i in 3" :key="i"></BookingItem>
+    </section>
+  </main>
+</template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+import BookingItem from '@/components/BookingItem.vue';
+</script>

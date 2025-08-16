3463b36d5b52108a83f67aaa179c5cd063a9c499
Elfonator <elfo.here@gmail.com>
2025-08-17 00:07:35 +0200

    feat(app): optimistic UI + error/empty states + responsive UI + dynamic icons

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,18 @@
   <main class="mx-auto max-w-screen-xl my-8 space-y-8 px-4">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
-      <template v-if="!eventsLoading">
-        <EventCard
-          v-for="event in events"
-          :key="event.id"
-          :title="event.title"
-          :when="event.date"
-          :description="event.description"
-          @register="handleRegistration(event)"
-        />
-      </template>
-
-      <template v-else>
-        <LoadingEventCard v-for="i in 4" :key="i"></LoadingEventCard>
-      </template>
-    </section>
-
+    <EventList @register="handleRegistration($event)" />
     <h2 class="text-2xl font-medium">Your Bookings</h2>
 
     <section class="grid grid-cols-1 gap-4">
       <template v-if="!bookingLoading">
-        <BookingItem v-for="booking in bookings" :key="booking.id" :title="booking.eventTitle" />
+        <BookingItem
+          v-for="booking in bookings"
+          :key="booking.id"
+          :title="booking.eventTitle"
+          :status="booking.status"
+          @cancelled="cancelBooking(booking.id)"
+        />
       </template>
 
       <template v-else>
@@ -35,25 +25,15 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import EventCard from '@/components/EventCard.vue';
-import BookingItem from '@/components/BookingItem.vue';
-import LoadingEventCard from '@/components/LoadingeventCard.vue';
-import LoadingBookingItem from '@/components/LoadingBookingItem.vue';
 
-const events = ref([]);
-const eventsLoading = ref(false);
+import BookingItem from '@/components/BookingItem.vue';
+import LoadingBookingItem from '@/components/LoadingBookingItem.vue';
+import EventList from '@/components/EventList.vue';
+
 const bookings = ref([]);
 const bookingLoading = ref(false);
 
-const fetchEvents = async () => {
-  eventsLoading.value = true;
-  try {
-    const response = await fetch('http://localhost:3001/events');
-    events.value = await response.json();
-  } finally {
-    eventsLoading.value = false;
-  }
-};
+const findBookingById = (id) => bookings.value.findIndex((b) => b.id === id);
 
 const fetchBookings = async () => {
   bookingLoading.value = true;
@@ -66,24 +46,61 @@ const fetchBookings = async () => {
 };
 
 const handleRegistration = async (event) => {
+  if (bookings.value.some((booking) => booking.eventId === event.id && booking.userId === 1)) {
+    alert('You are already registered for this event');
+    return;
+  }
+
   const newBooking = {
     id: Date.now().toString(),
     userId: 1,
     eventId: event.id,
     eventTitle: event.title,
+    status: 'pending',
   };
 
-  await fetch('http://localhost:3001/bookings', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      ...newBooking,
-      status: 'confirmed',
-    }),
-  });
+  bookings.value.push(newBooking);
+
+  try {
+    const response = await fetch('http://localhost:3001/bookings', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        ...newBooking,
+        status: 'confirmed',
+      }),
+    });
+
+    if (response.ok) {
+      const index = findBookingById(newBooking.id);
+      bookings.value[index] = await response.json();
+    } else {
+      throw Error('Failed to confirm booking');
+    }
+  } catch (e) {
+    console.error('Failed to register for event: ', e);
+    bookings.value = bookings.value.filter((b) => b.id !== newBooking.id); //remove the booking from the list because of the fail by filtering
+  }
 };
 
-onMounted(() => {
-  (fetchEvents(), fetchBookings());
-});
+const cancelBooking = async (bookingId) => {
+  const index = findBookingById(bookingId);
+  const originalBooking = bookings.value[index];
+  bookings.value.splice(index, 1);
+
+  try {
+    const response = await fetch(`http://localhost:3001/bookings/${bookingId}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw Error('Booking could not be cancelled.');
+    }
+  } catch (e) {
+    console.error('Failed to cancel booking: ', e);
+    bookings.value.splice(index, 0, originalBooking); //remove 0 but restore original element
+  }
+};
+
+onMounted(() => fetchBookings());
 </script>
--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -1,8 +1,12 @@
 <template>
   <SectionCard>
     <div class="flex justify-between">
-      <div>{{ title }}</div>
-      <RoundButton variant="danger" @click="$emit('cancel')"> Cancel </RoundButton>
+      <div class="flex space-x-2">
+        <div>{{ title }}</div>
+        <div><component :is="icon" :class="{ 'animate-spin': pending }"></component></div>
+      </div>
+
+      <RoundButton variant="danger" @click="$emit('cancelled')"> Cancel </RoundButton>
     </div>
   </SectionCard>
 </template>
@@ -10,8 +14,14 @@
 <script setup>
 import SectionCard from '@/components/SectionCard.vue';
 import RoundButton from '@/components/RoundButton.vue';
-defineProps({
+import { LoaderCircle, Check } from 'lucide-vue-next';
+import { computed } from 'vue';
+const props = defineProps({
   title: String,
+  status: String,
 });
-defineEmits(['cancel']);
+
+const pending = computed(() => props.status === 'pending');
+const icon = computed(() => (pending.value ? LoaderCircle : Check));
+defineEmits(['cancelled']);
 </script>

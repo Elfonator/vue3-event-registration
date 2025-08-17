d9394268f14e93bdc8694169fb45cb7596e6b4a0
Elfonator <elfo.here@gmail.com>
2025-08-17 11:03:56 +0200

    feat(app): composable bookings + new UI components + optimistic flow

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,11 +1,8 @@
 <template>
   <template v-if="error">
-    <SectionCard>
-      <div class="space-y-4 items-center flex flex-col">
-        <div class="text-red-500">Could not load events at the moment. Please try again.</div>
-        <RoundButton @click="fetchEvents">Retry now</RoundButton>
-      </div>
-    </SectionCard>
+    <ErrorCard :retry="fetchEvents">
+      Could not load events at the moment. Please try again.
+    </ErrorCard>
   </template>
 
   <template v-else>
@@ -18,7 +15,7 @@
             :title="event.title"
             :when="event.date"
             :description="event.description"
-            @register="$emit('register', event)"
+            @register="handleRegistration(event)"
           />
         </template>
         <template v-else>
@@ -37,10 +34,10 @@
 import { ref, onMounted } from 'vue';
 import EventCard from '@/components/EventCard.vue';
 import LoadingEventCard from '@/components/LoadingEventCard.vue';
-import SectionCard from '@/components/SectionCard.vue';
-import RoundButton from './RoundButton.vue';
+import ErrorCard from '@/components/ErrorCard.vue';
+import useBookings from '@/composables/useBookings';
 
-defineEmits(['register']);
+const { handleRegistration } = useBookings();
 
 const events = ref([]);
 const loading = ref(false);
